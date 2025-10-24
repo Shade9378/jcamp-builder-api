4ccc48b33637a201d0926676467f673812168f0e
Shade <anhvu_2027@depauw.edu>
2025-10-23 22:48:39 -0400

    Delete input endpoint

--- app.py
+++ app.py
@@ -1,46 +1,31 @@
 from flask import Flask, request
 from flask_cors import CORS
 from process import build_jcamp
-import gc
 import os
 
 app = Flask(__name__)
 CORS(app)
 
-level = 0
-correctAns = {}
-
-metadatas = {}
-assignments = []
-spec_jcamp = ""
-struc_jcamp = ""
-
 @app.route('/')
 def testing_route():
     return '<h1>Flask App is currently running</h1>'
+    
 
-
-@app.route('/app_input', methods=['POST'], strict_slashes=False)
-def get_input():
-    global assignments, spec_jcamp, struc_jcamp, metadatas
-
-    metadatas = request.json['metadatas']
-    assignments = request.json['assignments']
-    spec_jcamp = request.json['specJcamp']
-    struc_jcamp = request.json['strucJcamp']
-
-@app.route('/build_jcamp')
-def check_draw():
-    global assignments, spec_jcamp, struc_jcamp, metadatas
+@app.route('/build_jcamp', methods=['POST'])
+def build_jcamp_route():
+    data = request.json
+    metadatas = data.get('metadatas', {})
+    assignments = data.get('assignments', [])
+    spec_jcamp = data.get('specJcamp', '')
+    struc_jcamp = data.get('strucJcamp', '')
 
     built_jcamp = build_jcamp(metadatas, struc_jcamp, spec_jcamp, assignments)  # type: ignore
-    
+
     return built_jcamp
 
-
-@app.route('/identify_peak_and_build_jcamp')
-def identify_peak_and_build_jcamp():
-    return 
+@app.route('/identify_peak_and_build_jcamp', methods=['POST'])
+def identify_peak_and_build_jcamp_route():
+    return
 
 
 if __name__ == "__main__":
